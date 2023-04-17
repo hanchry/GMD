3f1acf95ece3ca387be22bc3d5852ed396d9b87c
nicolaecernean <nicolaecer@gmail.com>
2023-04-17 21:16:17 +0200

    added: player, controls and camera control

---
+++ NameToMake/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera cmVirtualCamera;
+    public Camera mainCamera;
+
+    private bool usingVirtualCam = true;
+    
+    public float zoomSpeed = 1f;
+    public float zoomMin = 1f;
+    public float zoomMax = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            usingVirtualCam = !usingVirtualCam;
+
+            if (usingVirtualCam)
+            {
+                cmVirtualCamera.gameObject.SetActive(true);
+            }
+            else
+            {
+                cmVirtualCamera.gameObject.SetActive(false);
+            }
+        }
+
+        if (!usingVirtualCam)
+        {
+            float x = Input.mousePosition.x;
+            float y = Input.mousePosition.y;
+
+            if (x < 10)
+            {
+                mainCamera.transform.position -= Vector3.left * (Time.deltaTime * 10);
+            }
+            else if (x > Screen.width - 10)
+            {
+                mainCamera.transform.position -= Vector3.right * (Time.deltaTime * 10);
+            }
+
+            if (y < 10)
+            {
+                mainCamera.transform.position -= Vector3.back * (Time.deltaTime * 10);
+            }
+            else if(y > Screen.height - 10)
+            {
+                mainCamera.transform.position -= Vector3.forward * (Time.deltaTime * 10);
+
+            }
+        }
+        
+        
+      
+    }
+}
