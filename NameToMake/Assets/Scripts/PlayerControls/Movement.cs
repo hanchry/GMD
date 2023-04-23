875cf7e9eb3db1681a362038fc7bbb5181901757
nicolaecernean <nicolaecer@gmail.com>
2023-04-23 22:14:33 +0200

    feat: finished health system implementation

--- NameToMake/Assets/Scripts/PlayerControls/Movement.cs
+++ NameToMake/Assets/Scripts/PlayerControls/Movement.cs
@@ -1,58 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-    
-public class Movement : MonoBehaviour
+
+namespace PlayerControls
 {
-    public NavMeshAgent agent;
-
-    public float rotateSpeedMovement = 0.05f;
-
-    private float rotateVelocity;
-
-    public Animator anim;
-
-    private float motionSmoothTime = 0.1f;
-    private static readonly int Speed = Animator.StringToHash("Speed");
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class Movement : MonoBehaviour
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
-    }
+        public NavMeshAgent agent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Animation();
-        Move();
-    }
+        public float rotateSpeedMovement = 0.05f;
 
-    public void Move()
-    {
-        if (Input.GetMouseButtonDown(1))
+        private float rotateVelocity;
+
+        public Animator anim;
+
+        private float motionSmoothTime = 0.1f;
+        private static readonly int Speed = Animator.StringToHash("Speed");
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            RaycastHit hit;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            agent = gameObject.GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Animation();
+            Move();
+        }
+
+        public void Move()
+        {
+            if (Input.GetMouseButtonDown(1))
             {
-                if (hit.collider.CompareTag("Ground"))
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
                 {
-                    agent.SetDestination(hit.point);
-                    agent.stoppingDistance = 0;
+                    if (hit.collider.CompareTag("Ground"))
+                    {
+                        agent.SetDestination(hit.point);
+                        agent.stoppingDistance = 0;
 
-                    Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
-                    float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationToLookAt.eulerAngles.y, ref rotateVelocity, rotateSpeedMovement* (Time.deltaTime*5));
+                        Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                        float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationToLookAt.eulerAngles.y, ref rotateVelocity, rotateSpeedMovement* (Time.deltaTime*5));
 
-                    transform.eulerAngles = new Vector3(0, rotationY, 0);
+                        transform.eulerAngles = new Vector3(0, rotationY, 0);
+                    }
                 }
             }
         }
-    }
-    public void Animation()
-    {
-        float speed = agent.velocity.magnitude/agent.speed;
-        anim.SetFloat(Speed, speed, motionSmoothTime, Time.deltaTime);
+        public void Animation()
+        {
+            float speed = agent.velocity.magnitude/agent.speed;
+            anim.SetFloat(Speed, speed, motionSmoothTime, Time.deltaTime);
+        }
     }
 }
---
+++ NameToMake/Assets/Scripts/PlayerControls/PlayerHealthCanvas.cs
@@ -0,0 +1,35 @@
+using DefaultNamespace;
+using Objects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayerControls
+{
+    public class PlayerHealthCanvas : MonoBehaviour
+    {
+       
+        private HealthSystem _healthSystem;
+        private Slider _slider;
+        public void Setup(HealthSystem healthSystem)
+        {
+            this._healthSystem = healthSystem;
+            healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
+        }
+        void Start()
+        {
+            _slider = GameObject.Find("HealthSlider").GetComponent<Slider>();
+        }
+
+        private void HealthSystem_OnHealthChanged(object sender, System.EventArgs e)
+        {
+            _slider.value = _healthSystem.GetHealthPercent();
+        }
+        // Update is called once per frame
+        void LateUpdate()
+        {
+          transform.LookAt(Camera.main.transform);
+          transform.Rotate(0,180,0 );
+        }
+        
+    }
+}
